a35298bcf56c6a6afdfda4af3b2db648945eac40
sukumaryadagiri <sukumaryadagiri@10.0.1.6>
2016-09-25 11:01:03 -0500

    Moved to Training branch

---
+++ Training/src/com/training/Helloworld.java
@@ -0,0 +1,11 @@
+package com.training;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld");
+		System.out.println("Helloworld. This is second Commit");
+	}
+
+}
