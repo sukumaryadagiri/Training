a0c12b263b7d1807f5645c9a2d5bb262e5ed7a60
sukumaryadagiri <sukumaryadagiri@192.168.15.9>
2017-08-04 21:13:23 -0500

    This is the third commit

--- Training/src/com/training/Helloworld.java
+++ Training/src/com/training/Helloworld.java
@@ -5,7 +5,8 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helloworld");
-		System.out.println("Helloworld. This is second Commit");
+		System.out.println("Helloworld. This is second Commit ");
+		System.out.println("May be the third commit : 08.04.2017");
 	}
 
 }
